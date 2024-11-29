9478ffc3acb1f07e9402820faaf12930e207a6d9
NICOLA NSENGIYUMVA <fatepepe66@gmail.com>
2024-11-29 15:05:48 +0200

    new commits

--- bookHandling/src/main/java/com/books/bookHandling/security/SecurityConfig.java
+++ bookHandling/src/main/java/com/books/bookHandling/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(withDefaults())
+                .cors(withDefaults ())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(
