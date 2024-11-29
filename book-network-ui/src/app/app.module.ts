0a301d813be6c779b8b07d0e60565c35158091a3
NICOLA NSENGIYUMVA <fatepepe66@gmail.com>
2024-11-29 15:56:05 +0200

    new commits

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule} from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import {LoginComponent} from './pages/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RegisterComponent} from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
 
   ],
   imports: [
