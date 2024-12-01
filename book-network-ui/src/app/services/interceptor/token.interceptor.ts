d48a2a33f3f90d53e28413fd228c0a301edd26b0
NICOLA NSENGIYUMVA <fatepepe66@gmail.com>
2024-12-01 09:39:58 +0200

    new commits

---
+++ book-network-ui/src/app/services/interceptor/token.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpInterceptorFn,
+  HttpRequest
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {TokenService} from '../token/token.service';
+
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
+
+  constructor(
+    private tokenService: TokenService
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this.tokenService.token;
+    if (token) {
+      const authReq = request.clone({
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        })
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(request);
+  }
+}
